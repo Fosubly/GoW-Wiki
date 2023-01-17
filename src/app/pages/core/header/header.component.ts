9c6ea6a602ce06f81a6ffd8e15a6f13936eb9219
Dzmitry Drabo <dzmitry.drabo@innowise-group.com>
2023-01-17 23:25:49 +0300

    update headers buttons

--- src/app/pages/core/header/header.component.ts
+++ src/app/pages/core/header/header.component.ts
@@ -1,5 +1,24 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
+const buttons = [
+  {
+    translateKey: 'header.characters',
+    path: '',
+  },
+  {
+    translateKey: 'header.items',
+    path: '',
+  },
+  {
+    translateKey: 'header.enemies',
+    path: '',
+  },
+  {
+    translateKey: 'header.quests',
+    path: '',
+  },
+];
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,6 +26,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
+  public buttons = buttons;
   constructor() {}
 
   ngOnInit(): void {}
